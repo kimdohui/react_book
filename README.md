51240c7e3c88ae65ba0489aeb0d32d11a12f8d89
Dohui Kim <51225575+kimdohui@users.noreply.github.com>
2020-06-11 15:46:05 +0900

    2020/06/11/TIL

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ---
 
-6/8 (p. 142 ~ 156 ) - ref 사용 , scrollBox
-6/9 (p. 157 ~ 188 ) - IterationSample(리스트 추가,삭제 가능) , LifeCycle
-6/10 (p. 189 ~ 215) Hooks
+- 6/8 (p. 142 ~ 156 ) - ref 사용 , scrollBox
+- 6/9 (p. 157 ~ 188 ) - IterationSample(리스트 추가,삭제 가능) , LifeCycle
+- 6/10 (p. 189 ~ 215) Hooks
+- 6/11 (p. 216 ~ 252) 컴포넌트 스타일링(styled-component)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from "react";
-import Average from "./6_10/Average";
+import React, { Component } from "react";
+import StyledComponent from "./6_11/StyledComponent";
 
-const App = () => {
-  /*const [visible, setVisible] = useState(false);
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
-  */
-  return <Average />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
+}
 
 export default App;
