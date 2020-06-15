2683a0ed6b44775c1eb6f3b433246c3a1024d437
Dohui Kim <51225575+kimdohui@users.noreply.github.com>
2020-06-15 22:02:06 +0900

    2020/06/15/URL 피라미터와 쿼리

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
-import React, {useRef, useCallback, useState} from 'react';
-import produce from 'immer';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import About from "./ReactRouter/About";
+import Home from "./ReactRouter/Home";
+import Profile from "./ReactRouter/Profile";
 
 const App = () => {
-  const nextId = useRef(1);
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
 
-  const [form , setForm] = useState({name : '', username : ''});
-  const [data, setData] = useState({
-    array : [],
-    uselessValue : null});
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
 
-    const onChange = useCallback(e => {
-      setForm(produce(form ,draft => {
-        draft[name] = value;
-      }));
-    }, []);
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
 
-    const onSubmit = useCallback(
-      e => {
-        e.preventDefault();
-        const info = {
-          id : nextId.current,
-          name : form.name,
-          username : form.username
-        };
-
-        setData(produce(data,draft => {
-          draft.array.push(info);
-        }));
-
-        setForm({name :'', username:''});
-        nextId.current += 1;
-      },
-      [form.name , form.username]
-    );
-
-    const onRemove = useCallback(
-      id => {
-        setData(
-          produce(data,draft => {
-            draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
-          })
-        );
-      },[]
-    );
-      return(...);
-}
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={["/about"]} component={About} exact={true} />
+      <Route path={"/profile/:username"} component={Profile} exact={true} />
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter } from "react-router-dom";
+//import "./index.css";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </BrowserRouter>,
+  document.getElementById("root")
 );
