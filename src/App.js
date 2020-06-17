aa7f7549d73817377113bb18b63e5679d47f6b99
Dohui Kim <51225575+kimdohui@users.noreply.github.com>
2020-06-17 20:24:21 +0900

    2020/06/17/TIL

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import React from "react";
-import { Route, Link, Switch } from "react-router-dom";
-import About from "./ReactRouter/About";
-import Home from "./ReactRouter/Home";
-import HistorySample from "./ReactRouter/HisrotySample";
-import Profiles from "./ReactRouter/Profiles";
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-
-        <li>
-          <Link to="/profiles">프로필</Link>
-        </li>
-
-        <li>
-          <Link to="/history">History예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path={["/about"]} component={About} exact={true} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재하지 않습니다.</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
         />
-      </Switch>
+      )}
     </div>
   );
 };
